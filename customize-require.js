7c18890bdf45aea31f3b60713de5e8551c43eb77
Jesse McCarthy <git_commits@jessemccarthy.net>
2016-04-07 15:50:44 -0400

    Refactor for `mods` array. Cache regexes.

--- customize-require.js
+++ customize-require.js
@@ -1,23 +1,44 @@
-var original = require.extensions[".js"];
+var handler = require.extensions[".js"];
+
+var patterns = {
+  dir: {},
+};
 
 function customize_require (opts) {
+  if (!(Array.isArray(opts.cfg.mods) && opts.cfg.mods.length)) return;
+
   var cfg = opts.cfg;
+  var mods = {};
+
+  opts.cfg.mods.forEach(function (mod) {
+    var type = mods[mod.type] = mods[mod.type] || {};
+    type[mod.from] = mod.to;
+  });
+
+  function make_custom_require (original) {
+    return function require (id) {
+      var alias;
+
+      if (mods.id && mods.id[id]) id = mods.id[id];
+      else {
+        Object.keys(mods.dir).some(function (dir) {
+          var pattern = patterns.dir[dir] = patterns.dir[dir] || RegExp(
+            "^(" + dir + ")(\/.+)"
+          );
+          var matches = id.match(pattern) || [];
+          alias = mods.dir[matches[1]];
+          if (alias) id = id.replace(matches[1], alias);
+
+          return !!alias;
+        });
+      }
+      return original.call(this, id);
+    };
+  }
 
   require.extensions[".js"] = function (module, pathname) {
-    var require = module.require;
-    module.require = function (id) {
-      var alias;
-      Object.keys(cfg.mods.dir).some(function (dir) {
-        var matches = id.match(/^(app)(\/.+)/) || [];
-        alias = cfg.mods.dir[matches[1]];
-        if (alias) id = id.replace(matches[1], alias);
-
-        return !!alias;
-      });
-
-      return require.call(this, id);
-    };
-    return original.call(this, module, pathname);
+    module.require = make_custom_require(module.require);
+    return handler.call(this, module, pathname);
   };
 }
 
